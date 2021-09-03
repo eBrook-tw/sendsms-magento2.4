7958ef70ce09b8a799bdb4edec2f5e42c8ebfd9b
Grass Huang <grass.huang@ebrook.com.tw>
2021-09-03 18:02:11 +0800

    Updated: fixed use virtaul type to confiure plugin, precompilation can not generate interceptor method.

---
+++ Plugin/Comment/InvoiceCommentSMSSender.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Ebrook
+ *
+ * @category    Ebrook
+ * @package     AnyPlaceMedia_SendSMS
+ * @copyright   Copyright © 2021 Ebrook co., ltd. (https://www.ebrook.com.tw)
+ * @source https://github.com/sendSMS-RO/sendsms-magento2.4
+ */
+
+namespace AnyPlaceMedia\SendSMS\Plugin\Comment;
+
+use AnyPlaceMedia\SendSMS\Plugin\NotifySMSSender;
+
+class InvoiceCommentSMSSender extends NotifySMSSender
+{
+
+}
---
+++ Plugin/Entity/ShipmentSMSSender.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Ebrook
+ *
+ * @category    Ebrook
+ * @package     AnyPlaceMedia_SendSMS
+ * @copyright   Copyright © 2021 Ebrook co., ltd. (https://www.ebrook.com.tw)
+ * @source https://github.com/sendSMS-RO/sendsms-magento2.4
+ */
+
+namespace AnyPlaceMedia\SendSMS\Plugin\Entity;
+
+use AnyPlaceMedia\SendSMS\Plugin\SMSSender;
+
+class ShipmentSMSSender extends SMSSender
+{
+
+}
